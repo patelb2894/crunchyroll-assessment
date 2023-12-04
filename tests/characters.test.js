f5d30d9a4f914cd081d8aaaec07f17d8f61e13e6
patelb2894 <151471182+patelb2894@users.noreply.github.com>
2023-12-03 18:31:46 -0800

    Adding error handling to the tests

--- tests/characters.test.js
+++ tests/characters.test.js
@@ -4,34 +4,45 @@ const axios = require('axios');
 
 describe('Star Wars Characters Test', () => {
   it('should retrieve a list of all Star Wars characters', async () => {
-    const response = await getCharacters();
-    expect(response.status).to.equal(200);
-    expect(response.data.results).to.be.an('array').that.is.not.empty;
-  });
-});
+    try {
+      const response = await getCharacters();
+      
+      // Verify that the response status is 200
+      expect(response.status).to.equal(200);
 
-describe('Star Wars Character Details Test', () => {
+      // Verify that at least one character is returned
+      expect(response.data.results).to.be.an('array').that.is.not.empty;
+    } catch (error) {
+      console.error('An error occurred while fetching characters:', error.message);
+      throw error;
+    }
+  });
+
   it('should retrieve details for a specific Star Wars character', async () => {
     // Specify the name of the character (e.g., Luke Skywalker)
     const characterName = 'Luke Skywalker';
 
-    // Make a request to the Star Wars API for the specific character
-    const response = await axios.get(`https://swapi.dev/api/people/?search=${encodeURIComponent(characterName)}`);
+    try {
+      // Make a request to the Star Wars API for the specific character
+      const response = await axios.get(`https://swapi.dev/api/people/?search=${encodeURIComponent(characterName)}`);
 
-    // Verify that the response status is 200
-    expect(response.status).to.equal(200);
+      // Verify that the response status is 200
+      expect(response.status).to.equal(200);
 
-    // Verify that only one result is returned
-    expect(response.data.count).to.equal(1);
+      // Verify that only one result is returned
+      expect(response.data.count).to.equal(1);
 
-    // Verify that the returned character details match the expected details
-    const character = response.data.results[0];
-    expect(character.name).to.equal(characterName);
-    // Add more assertions based on the expected details of the character
-    expect(character).to.have.property('height', '172');
-    expect(character).to.have.property('hair_color', 'blond');
-    // Add more assertions as needed
+      // Verify that the returned character details match the expected details
+      const character = response.data.results[0];
+      expect(character.name).to.equal(characterName);
+      expect(character).to.have.property('height', '172');
+      expect(character).to.have.property('hair_color', 'blond');
+    } catch (error) {
+      console.error(`An error occurred while fetching details for ${characterName}:`, error.message);
+      throw error;
+    }
   });
 });
+
 
 
--- tests/films.test.js
+++ tests/films.test.js
@@ -3,28 +3,36 @@ const { expect } = require('chai');
 
 describe('Star Wars Films Test', () => {
   it('should retrieve a list of all Star Wars films', async () => {
-    const response = await axios.get('https://swapi.dev/api/films/');
+    try {
+      const response = await axios.get('https://swapi.dev/api/films/');
 
-    // Verify that the response status is 200
-    expect(response.status).to.equal(200);
+      // Verify that the response status is 200
+      expect(response.status).to.equal(200);
 
-    // Verify that at least one film is returned
-    expect(response.data.results).to.be.an('array').that.is.not.empty;
-    const firstFilm = response.data.results[0];
-    expect(firstFilm).to.have.property('title', 'A New Hope');
-    expect(firstFilm).to.have.property('director', 'George Lucas');
-
+      // Verify that at least one film is returned
+      expect(response.data.results).to.be.an('array').that.is.not.empty;
+      const firstFilm = response.data.results[0];
+      expect(firstFilm).to.have.property('title', 'A New Hope');
+      expect(firstFilm).to.have.property('director', 'George Lucas');
+    } catch (error) {
+      console.error('An error occurred while fetching films:', error.message);
+      throw error;
+    }
   });
 
   it('should retrieve details for a specific Star Wars film', async () => {
     const filmTitle = 'A New Hope';
-    const response = await axios.get(`https://swapi.dev/api/films/?search=${encodeURIComponent(filmTitle)}`);
+    try {
+      const response = await axios.get(`https://swapi.dev/api/films/?search=${encodeURIComponent(filmTitle)}`);
 
-    // Verify that the response includes the correct film details, and that only one result is returned.
-    expect(response.status).to.equal(200);
-    expect(response.data.count).to.equal(1);
-    const film = response.data.results[0];
-    expect(film.title).to.equal(filmTitle);
+      // Verify that the response includes the correct film details, and that only one result is returned.
+      expect(response.status).to.equal(200);
+      expect(response.data.count).to.equal(1);
+      const film = response.data.results[0];
+      expect(film.title).to.equal(filmTitle);
+    } catch (error) {
+      console.error(`An error occurred while fetching details for ${filmTitle}:`, error.message);
+      throw error;
+    }
   });
 });
-
